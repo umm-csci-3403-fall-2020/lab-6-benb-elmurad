8a078552d1b84744a0d0cbd5f04394703d87df6d
Ben <burge337@morris.umn.edu>
2020-10-14 18:27:58 -0500

    Initial poking around at how to make threads in java
    We don't really understand how to create and call threads yet, or what's going on with the start method that's called
    from main, so this commit is mostly us just flailing around a bit.
    
    Based off of our reading, it seems like we need to make some class that implements runnable, that then contains a run
    method that performs the usual reading or writing activity we would normally do.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -17,7 +17,29 @@ public class EchoClient {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
+		
+		Thread clientRead = new Thread (){
+			try {
 
+		int echoThisByte;
+		
+		// read from System.in and send that info to the server 1 byte at a time.
+		while ((echoThisByte = System.in.read()) != 1) {
+			// write the byte to the outputstream
+			socketOutputStream.write(echoThisByte);
+			// send it to the server
+			socketOutputStream.flush();
+		}
+		socket.shutdownOutput();
+		System.out.flush();
 		// Put your code here.
 	}
+	catch (ConnectException ce) {
+		System.out.println("We were unable to connect to the host");
+		System.out.println("You should make sure the server is running.");
+	} catch (IOException ioe) {
+		System.out.println("We caught an unexpected exception");
+		System.err.println(ioe);
+	
 }
+		};

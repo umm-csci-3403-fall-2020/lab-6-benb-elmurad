e69038d61818972420aa96a4128de955debbc1fa
Benjamin Burgess <burge337@morris.umn.edu>
2020-10-15 11:05:56 -0500

    First attempt at multi-threaded client
    Haven't actually tested this because this was written from vscode and we don't have bats there. Pushing so we can grab the changes in vim and test them.
    
    Added two new classes that extend Thread: GetOutgoingThread and GetIncomingThread that handle sending and receiving things like you'd expect.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -3,6 +3,7 @@ package echoserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 
 public class EchoClient {
@@ -10,36 +11,90 @@ public class EchoClient {
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
+		// public String server;
+		// if (args.length == 0) {
+		// server = "127.0.0.1";
+		// } else {
+		// server = args[0];
+		// }
 		client.start();
+
 	}
 
-	private void start() throws IOException {
+	public void start() throws IOException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
-		
-		Thread clientRead = new Thread (){
-			try {
 
-		int echoThisByte;
-		
-		// read from System.in and send that info to the server 1 byte at a time.
-		while ((echoThisByte = System.in.read()) != 1) {
-			// write the byte to the outputstream
-			socketOutputStream.write(echoThisByte);
-			// send it to the server
-			socketOutputStream.flush();
-		}
-		socket.shutdownOutput();
-		System.out.flush();
-		// Put your code here.
+		// create our thread that sends keyboard input to the server
+		GetOutgoingThread clientSend = new GetOutgoingThread("clientSend", socket, socketOutputStream);
+		clientSend.run();
+		// create our thread that receives that input echoed back from the server
+		GetIncomingThread clientReceive = new GetIncomingThread("clientReceive", socket, socketInputStream);
+		clientReceive.run();
 	}
-	catch (ConnectException ce) {
-		System.out.println("We were unable to connect to the host");
-		System.out.println("You should make sure the server is running.");
-	} catch (IOException ioe) {
-		System.out.println("We caught an unexpected exception");
-		System.err.println(ioe);
-	
+
+	public class GetOutgoingThread extends Thread {
+		public Thread inputThread;
+		public String threadName;
+		public Socket socketName;
+		public OutputStream outputStreamName;
+		public InputStream inputStreamName;
+
+		GetOutgoingThread(String name, Socket socket, OutputStream outputStream) {
+			threadName = name;
+			socketName = socket;
+			outputStreamName = outputStream;
+			// System.out.println("Creating " + threadName);
+		}
+
+		public void run() {
+			try {
+				int echoThisByte;
+				while ((echoThisByte = System.in.read()) != -1) {
+					// write the byte to the outputstream
+					outputStreamName.write(echoThisByte);
+					// send it off to the server
+					outputStreamName.flush();
+				}
+				// tell the server we are done sending it stuff
+				socketName.shutdownOutput();
+			} catch (ConnectException ce) {
+				System.out.println("We were unable to connect to localhost.");
+			} catch (IOException ioe) {
+				System.out.println("We caught an unexpected exception");
+				System.out.println(ioe);
+			}
+		}
+	}
+	public class GetIncomingThread extends Thread {
+		public Thread outputThread;
+		public String threadName;
+		public Socket socketName;
+		public InputStream inputStreamName;
+
+		GetIncomingThread(String name, Socket socket, InputStream inputStream){
+			threadName = name;
+			socketName = socket;
+			inputStreamName = inputStream;
+		}
+
+		public void run() {
+			try {
+				int echoThisByte;
+				while ((echoThisByte = inputStreamName.read()) != -1) {
+					// write the byte we just read to the outputStream
+					System.out.write(echoThisByte);
+					// send it back to the client
+					System.out.flush();
+				}
+			} catch (ConnectException ce) {
+				System.out.println("We were unable to connect to localhost.");
+			} catch (IOException ioe) {
+				System.out.println("We caught an unexpected exception");
+				System.out.println(ioe);
+			}
+		}
+	}
+
 }
-		};
